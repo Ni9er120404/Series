8d73be6e078136bf36acf8f0a85b9b2b22682788
Даниил Лямин <Niger120404@yandex.ru>
2023-04-11 15:30:27 +0300

    The client side of the project has been created

---
+++ MoviesAndSeries.Client/AuthorizationPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MoviesAndSeries.Client
+{
+	public partial class AuthorizationPage : ContentPage
+	{
+		public AuthorizationPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void AuthorizationButtonClicked(object sender, EventArgs e)
+		{
+			HttpClient httpClient = new();
+
+			string baseUrl = "https://localhost:32768/Authorization";
+			string email = EmailEntry.Text;
+			string password = PasswordEntry.Text;
+			string url = $"{baseUrl}?email={email}&password={password}";
+
+			HttpResponseMessage response = await httpClient.GetAsync(url);
+
+			_ = response.EnsureSuccessStatusCode();
+		}
+
+		private void RegistrationButtonClicked(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
