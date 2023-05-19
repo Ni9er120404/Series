659a22439f73c16f47a093e4b955e4a860a4b8a5
Vladislav <vgrechannik@gmail.com>
2023-05-20 02:46:05 +0300

    A minor tweak to the profile page

--- MoviesAndSeries.Client/Profile.xaml.cs
+++ MoviesAndSeries.Client/Profile.xaml.cs
@@ -2,8 +2,15 @@ namespace MaSMAUI;
 
 public partial class Profile : ContentPage
 {
+
+	private uint GetHoursWatched()
+	{
+		return 0;
+	}
+
 	public Profile()
 	{
 		InitializeComponent();
+		SpentHours.Text = GetHoursWatched().ToString();
 	}
 }
