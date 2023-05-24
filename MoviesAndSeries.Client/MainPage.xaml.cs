c0966b956383ebe85499db4a4057885d3c355379
Владислав Гречанник <m2108502@edu.misis.ru>
2023-05-24 15:23:41 +0300

    Remove login page

--- MoviesAndSeries.Client/MainPage.xaml.cs
+++ MoviesAndSeries.Client/MainPage.xaml.cs
@@ -1,24 +1,11 @@
-﻿using System.Text.RegularExpressions;
-
-namespace MaSMAUI;
+﻿namespace MaSMAUI;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-	}
-
-	private async void Login(object sender, EventArgs e)
-	{
-        // Navigate to page AfterLogin
-        await Navigation.PushAsync(new AfterLogin()); 
+        Navigation.PushAsync(new AfterLogin());
     }
 }
-
-internal readonly struct LoginBinding {
-	public static string EmailPattern => new GeneratedRegexAttribute("""(?<name>\w*)@(?<domain>(?<domainname>\w*).(?<domainarea>\w*))""").Pattern;
-	public LoginBinding() { }
-}
-
 
