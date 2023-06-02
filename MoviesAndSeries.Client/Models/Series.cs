b41cf164c3262a74537eed3aad9cb58adcc0db82
Vladislav <vgrechannik@gmail.com>
2023-06-02 08:59:05 +0300

    update: make series look more like what comes from api

--- MoviesAndSeries.Client/Models/Series.cs
+++ MoviesAndSeries.Client/Models/Series.cs
@@ -6,22 +6,38 @@ using System.Threading.Tasks;
 
 namespace MaSMAUI.Models
 {
-	public struct Series
+	public class Series
 	{
 		public string Name { get; set; }
 
-		public string? Description { get; set; }
+		public string Rating { get; set; }
 
-		public double Rating { get; set; }
+		public string StartDate { get; set; }
 
-		public int NumberOfSeasons { get; set; }
-
-		public DateTime StartDate { get; set; }
-
-		public DateTime EndDate { get; set; }
+		public string EndDate { get; set; }
 
 		public Uri Poster { get; set; }
 
-		public uint WatchCounter { get; set; }
+		public long WatchCounter { get; set; } = 0;
+
+		public Series(Org.OpenAPITools.Model.Series series)
+		{
+			Name = series.Name;
+			Rating = series.ImdbRating;
+			StartDate = series.StartDate;
+			EndDate = series.EndDate;
+			Poster = new(series.Poster);
+		}
+
+		public void AddOneView()
+		{
+			WatchCounter++;
+		}
+
+		public void DeleteOneView()
+		{
+			if (WatchCounter > 0)
+				WatchCounter--;
+		}
 	}
 }
