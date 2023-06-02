dea470506bddac9b737003b5d7c0c3786eedc389
Vladislav <vgrechannik@gmail.com>
2023-06-02 09:00:47 +0300

    update: add some styles to series card

--- MoviesAndSeries.Client/Serial.xaml.cs
+++ MoviesAndSeries.Client/Serial.xaml.cs
@@ -10,4 +10,35 @@ public partial class Serial : ContentView
 	{
 		InitializeComponent();
 	}
+
+	private void AddOneView()
+	{
+		Series?.AddOneView();
+	}
+
+	private void DeleteOneView()
+	{
+		Series?.DeleteOneView();
+	}
+
+	private void UpdateAmount()
+	{
+		if (Series is not null)
+		{
+			Amount.Text = Series.WatchCounter.ToString();
+			ApiConnection.GetInstance().Api.ApiSeriesNameQuantityPost(Series.Name, Series.WatchCounter);
+		}
+	}
+
+	private void Add_Button_Clicked(object sender, EventArgs e)
+	{
+		AddOneView();
+		UpdateAmount();
+	}
+
+	private void Minus_Button_Clicked(object sender, EventArgs e)
+	{
+		DeleteOneView();
+		UpdateAmount();
+	}
 }
