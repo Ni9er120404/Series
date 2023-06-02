9bffe36c759cee51c326415d5e7ff00d0ee0f916
Vladislav <vgrechannik@gmail.com>
2023-06-02 08:58:39 +0300

    update: add api to the project

---
+++ MoviesAndSeries.Client/ApiConnection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.OpenAPITools.Api;
+using Org.OpenAPITools.Client;
+using Org.OpenAPITools.Model;
+
+namespace MaSMAUI
+{
+	internal class ApiConnection
+	{
+		private static readonly Lazy<ApiConnection> instance = new(() => {
+			var api = new ApiConnection();
+			return api;
+		});
+
+		public SeriesApi Api { get; private set; }
+
+		private ApiConnection()
+		{
+			Configuration configuration = new()
+			{
+				BasePath = "http://mas.vladexa.ru"
+			};
+			Api = new SeriesApi(configuration);
+		}
+
+		public static ApiConnection GetInstance()
+		{
+			return instance.Value;
+		}
+	}
+}
